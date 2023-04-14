ea6ea8af5d9f636d9afac20720b63768f39ebcbd
drew-chidi <chidi.andrew@gmail.com>
2023-04-14 17:32:32 +0100

    implemented mobile navigation

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,10 +2,18 @@ import Image from 'next/image';
 import { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { RiHome5Line, RiFileUploadLine } from 'react-icons/ri';
+import { BsCartDash, BsChevronUp, BsChevronDown } from 'react-icons/bs';
 import PageLink from './PageLink';
+import Link from 'next/link';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [hideUploadList, setHideUploadList] = useState(true);
+
+  const handleUploadListToggle = () => {
+    setHideUploadList((value) => !value);
+  };
 
   const toggleHandler = () => {
     setOpen((value) => !value);
@@ -45,36 +53,128 @@ const Navbar = () => {
             )}
             <nav>
               <ul className='flex flex-col gap-8'>
-                <li className=''>
-                  <PageLink
-                    class=''
-                    page='Home'
-                    //   selectedPage={selectedPage}
-                    //   setSelectedPage={setSelectedPage}
-                  />
+                <li className=' flex item-center gap-[17px]'>
+                  <div className='flex items-center'>
+                    <RiHome5Line />
+                  </div>
+                  <Link
+                    href='/'
+                    // className={`${
+                    //   selectedPage === lowerCasePage ? 'text-blue' : ''
+                    // } hover:text-blue-50`}
+                    // onClick={() => setSelectedPage(lowerCasePage)}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className=' flex item-center gap-[17px]'>
+                  <div className='flex items-center'>
+                    <BsCartDash />
+                  </div>
+                  <Link
+                    href='/'
+                    // className={`${
+                    //   selectedPage === lowerCasePage ? 'text-blue' : ''
+                    // } hover:text-blue-50`}
+                    // onClick={() => setSelectedPage(lowerCasePage)}
+                  >
+                    Cart
+                  </Link>
+                </li>
+                <li
+                  className='flex item-center flex-col relative'
+                  onClick={handleUploadListToggle}
+                >
+                  <div className='flex  items-center  gap-[17px]'>
+                    <RiFileUploadLine fill='white' color='white' />
+                    <div className='flex items-center'>
+                      <Link
+                        href='/'
+                        // className={`${
+                        //   selectedPage === lowerCasePage ? 'text-blue' : ''
+                        // } hover:text-blue-50`}
+                        // onClick={() => setSelectedPage(lowerCasePage)}
+                      >
+                        Upload
+                      </Link>
+                      <div className='ml-[10px] flex items-center'>
+                        {hideUploadList ? <BsChevronDown /> : <BsChevronUp />}
+                      </div>
+                    </div>
+                  </div>
+                  {/* SUB LISTS */}
+                  {!hideUploadList && (
+                    <ul className='pl-8 mt-3.5'>
+                      <li className='mb-3'>
+                        <Link href='/new-upload'>New Upload</Link>
+                      </li>
+                      <li>
+                        <Link href='/myuploads'>My Uploads</Link>
+                      </li>
+                    </ul>
+                  )}
                 </li>
                 <li className=''>
-                  <PageLink
-                    class=''
-                    page='About'
-                    //   selectedPage={selectedPage}
-                    //   setSelectedPage={setSelectedPage}
-                  />
+                  <Link
+                    href='/'
+                    // className={`${
+                    //   selectedPage === lowerCasePage ? 'text-blue' : ''
+                    // } hover:text-blue-50`}
+                    // onClick={() => setSelectedPage(lowerCasePage)}
+                  >
+                    Signin
+                  </Link>
                 </li>
                 <li className=''>
-                  <PageLink
-                    class=''
-                    page=' Portfolio'
-                    //   selectedPage={selectedPage}
-                    //   setSelectedPage={setSelectedPage}
-                  />
+                  <Link
+                    href='/'
+                    className='px-4 py-2 text-xs border rounded-lg'
+                    // className={`${
+                    //   selectedPage === lowerCasePage ? 'text-blue' : ''
+                    // } hover:text-blue-50`}
+                    // onClick={() => setSelectedPage(lowerCasePage)}
+                  >
+                    Signup
+                  </Link>
                 </li>
-                <li className=''>
-                  <PageLink
-                    page='Contact'
-                    //   selectedPage={selectedPage}
-                    //   setSelectedPage={setSelectedPage}
-                  />
+                <li
+                  className='flex item-center flex-col relative'
+                  onClick={handleUploadListToggle}
+                >
+                  <div className='flex  items-center  gap-[17px]'>
+                    <Image
+                      src='/assets/images/person.png'
+                      width={24}
+                      height={24}
+                      alt='user'
+                      className='rounded-full'
+                    />{' '}
+                    <div className='flex items-center'>
+                      <Link
+                        href='/'
+                        // className={`${
+                        //   selectedPage === lowerCasePage ? 'text-blue' : ''
+                        // } hover:text-blue-50`}
+                        // onClick={() => setSelectedPage(lowerCasePage)}
+                      >
+                        Hi, Muyiwa
+                      </Link>
+                      <div className='ml-[10px] flex items-center'>
+                        {hideUploadList ? <BsChevronDown /> : <BsChevronUp />}
+                      </div>
+                    </div>
+                  </div>
+                  {/* SUB LISTS */}
+                  {!hideUploadList && (
+                    <ul className='pl-8 mt-3.5'>
+                      <li className='mb-3'>
+                        <Link href='/new-upload'>New Upload</Link>
+                      </li>
+                      <li>
+                        <Link href='/myuploads'>My Uploads</Link>
+                      </li>
+                    </ul>
+                  )}
                 </li>
               </ul>
             </nav>
