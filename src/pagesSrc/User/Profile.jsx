28b0014296a955941a306aadabf7914f96bc3c17
hakikicode <57021097+hakikicode@users.noreply.github.com>
2023-07-13 10:13:33 +0100

    Update Profile.jsx

--- src/pagesSrc/User/Profile.jsx
+++ src/pagesSrc/User/Profile.jsx
@@ -19,11 +19,7 @@ const Profile = () => {
             <p className='text-xl'>Edit</p>
           </div>
           <p className='mb-7'>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni .
+            THIS IS YOUR PROFILE PAGE .
           </p>
 
           {/* Contact Details: Locations and Social Links */}
