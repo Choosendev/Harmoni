580bd5478298705d497cb1bc3b8357f9de8f9950
Horlarmmy <aladetoheeb5@gmail.com>
2023-07-26 01:24:14 +0100

    Feat: Add connect wallet button

--- client/src/components/Header/Navbar.jsx
+++ client/src/components/Header/Navbar.jsx
@@ -12,6 +12,9 @@ import { logout, reset } from '../../states/slices/authSlice';
 const Navbar = ({ page }) => {
   const [open, setOpen] = useState(false);
   const [hideUploadList, setHideUploadList] = useState(true);
+  
+  const [isConnected, setIsConnected] = useState(false);
+  const [account, setAccount] = useState(null);
   const [showProfileOptions, setProfileOptions] = useState(false);
 
   const router = useRouter();
@@ -34,6 +37,59 @@ const Navbar = ({ page }) => {
     dispatch(reset());
     router.push('/');
   };
+
+  const connectWallet = async() => {
+    try {
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x1F' }],
+      })
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+        params: [
+          {
+            chainId: "0x1F",
+            chainName: "RSK Testnet",
+            rpcUrls: ["https://public-node.testnet.rsk.co"],
+            blockExplorerUrls: ["https://explorer.testnet.rsk.co"],
+            nativeCurrency: {
+              name: "",
+              symbol: "tRBTC",
+              decimals: 18,
+            },
+          },
+        ],
+      });
+      setIsConnected(true);
+      const account = await accounts[0];
+      setAccount(account);
+   
+      // console.log(account);
+    } catch (switchError) {
+      if (switchError.code === 4902) {
+        try {
+            await window.ethereum.request({
+                method: 'wallet_addEthereumChain',
+                params: [
+                    {
+                        chainId: '0x1F',
+                        chainName: 'RSK Testnet',
+                        nativeCurrency: {
+                            name: 'tRBTC',
+                            symbol: 'tRBTC',
+                            decimals: 18,
+                        },
+                        rpcUrls: ['https://public-node.testnet.rsk.co'],
+                    },
+                ],
+            });
+        } catch (addError) {
+            // handle "add" error
+            console.log(addError);
+        }
+      }
+    }
+  }
 
   return (
     <div className=' fixed top-0 left-0 w-full z-10'>
@@ -151,7 +207,6 @@ const Navbar = ({ page }) => {
                 <li className=''>
                   <Link
                     href='/signup'
-                    className='px-4 py-2 text-xs border rounded-lg'
                     className={`${
                     selectedPage === lowerCasePage ? 'text-blue' : ''
                     } hover:text-blue-50`}
@@ -324,15 +379,14 @@ const Navbar = ({ page }) => {
                 Signup
               </Link>
             </li>
-            <Link
-                    href='/walletconnect'
-                    className={`${
-                    selectedPage === lowerCasePage ? 'text-blue' : ''
-                    } hover:text-blue-50`}
-                    onClick={() => setSelectedPage(lowerCasePage)}
+            <li
+                    className='px-4 py-2 text-xs border rounded-lg'
+                    // className={`${
+                    // selectedPage === lowerCasePage ? 'text-blue' : ''
+                    // } hover:text-blue-50`}
+                    onClick={() => connectWallet()}
                   >
-                    WalletConnect
-                  </Link>
+                    {isConnected ? `${account.slice(0, 8)}...` : "Connect Wallet"}
                 </li>
             <li
               className='flex item-center flex-col relative'
